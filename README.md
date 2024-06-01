afece58f664bb3efa5116b36c98ef081aedcf252
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2024-06-01 19:35:15 -0400

    feat: Create next.js app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# convert
 Convert any image to JPG at https://convert.jinshub.com. Fast, secure, ad-free.
+
+Files are converted in-browser for privacy.
+
+If you like the project, please consider [buying me a coffee.](https://www.buymeacoffee.com/jayjin)
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Convert Any Image to JPG",
+  description: "Convert any image to JPG. Fast, secure, ad-free. Files are converted in-browser for privacy.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
